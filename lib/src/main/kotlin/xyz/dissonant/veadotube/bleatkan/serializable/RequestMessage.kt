d3a4b0ecf1cd333a6e1190bc17e05277574ae889
DissonantAU <33020221+DissonantAU@users.noreply.github.com>
2024-08-27 23:42:29 +1000

    Moved Vt Message Serializers to Separate File Rearranged RequestMessage.kt

--- lib/src/main/kotlin/xyz/dissonant/veadotube/bleatkan/serializable/RequestMessage.kt
+++ lib/src/main/kotlin/xyz/dissonant/veadotube/bleatkan/serializable/RequestMessage.kt
@@ -6,138 +6,6 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
 
 
-// Enum for Known Message Events
-enum class MessageEvent(val value: String) {
-    UNKNOWN("UNKNOWN"),
-    ERROR("ERROR"),
-    /* Message is targeting a specific node */
-    /** For a Request that targets a node (eg stateEvents)*/
-    @SerialName("payload")
-    PAYLOAD("payload"),
-
-    /* Requesting Values*/
-    /** Requests a List of Possible Values*/
-    @SerialName("list")
-    LIST("list"),
-    ;
-
-    companion object {
-        /**
-         * Gets Enum Constant with given Value
-         *
-         * Returns UNKNOWN ENUM if not found
-         *
-         *@return Enum Constant
-         */
-        @JvmStatic
-        fun fromValue(value: String): MessageEvent {
-            return entries.find { it.value == value } ?: UNKNOWN
-        }
-    }
-}
-
-
-// Enum for Known Message Types
-enum class MessagePayloadType(val value: String) {
-    UNKNOWN("UNKNOWN"),
-
-    @SerialName("stateEvents")
-    STATE_EVENTS("stateEvents"),
-    ;
-
-    companion object {
-        /**
-         * Gets Enum Constant with given Value
-         *
-         * Returns UNKNOWN ENUM if not found
-         *
-         *@return Enum Constant
-         */
-        @JvmStatic
-        fun fromValue(value: String): MessagePayloadType {
-            return entries.find { it.value == value } ?: UNKNOWN
-        }
-    }
-}
-
-
-// Enum for Known Message IDs
-enum class MessagePayloadId(val value: String) {
-    UNKNOWN("UNKNOWN"),
-
-    @SerialName("mini")
-    MINI("mini"),
-    ;
-
-    companion object {
-        /**
-         * Gets Enum Constant with given Value
-         *
-         * Returns UNKNOWN ENUM if not found
-         *
-         *@return Enum Constant
-         */
-        @JvmStatic
-        fun fromValue(value: String): MessagePayloadId {
-            return entries.find { it.value == value } ?: UNKNOWN
-        }
-    }
-}
-
-// Enum for Known Payload Events
-enum class PayloadEvent(val value: String) {
-    UNKNOWN("UNKNOWN"),
-
-    /* Requesting Values*/
-    /** Requests a List of Possible Values*/
-    @SerialName("list")
-    LIST("list"),
-
-    /** Requests current Single Value*/
-    @SerialName("peek")
-    PEEK("peek"),
-
-    /** Requests the image related to a Possible Value*/
-    @SerialName("thumb")
-    THUMB("thumb"),
-
-    /* Setting Values */
-    /** Sets a Single Value*/
-    @SerialName("set")
-    SET("set"),
-
-    /** Pushes a Single Value*/
-    @SerialName("push")
-    PUSH("push"),
-
-    /** Pops the last Pushed Single Value*/
-    @SerialName("pop")
-    POP("pop"),
-
-    /* Listening to Value Changes */
-    /** Listens to changes to a Channel Value*/
-    @SerialName("listen")
-    LISTEN("listen"),
-
-    /** Stops listening to changes to a Channel Value*/
-    @SerialName("unlisten")
-    UNLISTEN("unlisten"),
-    ;
-
-    companion object {
-        /**
-         * Gets Enum Constant with given Value
-         *
-         * Returns UNKNOWN ENUM if not found
-         *
-         *@return Enum Constant
-         */
-        @JvmStatic
-        fun fromValue(value: String): PayloadEvent {
-            return requireNotNull(entries.find { it.value == value }) { UNKNOWN }
-        }
-    }
-}
 
 /**
  * Factory for Building and Validating Request Messages.
@@ -632,6 +500,142 @@ class VtRequest {
     }
 
 }
+
+
+
+// Enum for Known Message Events
+enum class MessageEvent(val value: String) {
+    UNKNOWN("UNKNOWN"),
+    ERROR("ERROR"),
+    /* Message is targeting a specific node */
+    /** For a Request that targets a node (eg stateEvents)*/
+    @SerialName("payload")
+    PAYLOAD("payload"),
+
+    /* Requesting Values*/
+    /** Requests a List of Possible Values*/
+    @SerialName("list")
+    LIST("list"),
+    ;
+
+    companion object {
+        /**
+         * Gets Enum Constant with given Value
+         *
+         * Returns UNKNOWN ENUM if not found
+         *
+         *@return Enum Constant
+         */
+        @JvmStatic
+        fun fromValue(value: String): MessageEvent {
+            return entries.find { it.value == value } ?: UNKNOWN
+        }
+    }
+}
+
+
+// Enum for Known Message Types
+enum class MessagePayloadType(val value: String) {
+    UNKNOWN("UNKNOWN"),
+
+    @SerialName("stateEvents")
+    STATE_EVENTS("stateEvents"),
+    ;
+
+    companion object {
+        /**
+         * Gets Enum Constant with given Value
+         *
+         * Returns UNKNOWN ENUM if not found
+         *
+         *@return Enum Constant
+         */
+        @JvmStatic
+        fun fromValue(value: String): MessagePayloadType {
+            return entries.find { it.value == value } ?: UNKNOWN
+        }
+    }
+}
+
+
+// Enum for Known Message IDs
+enum class MessagePayloadId(val value: String) {
+    UNKNOWN("UNKNOWN"),
+
+    @SerialName("mini")
+    MINI("mini"),
+    ;
+
+    companion object {
+        /**
+         * Gets Enum Constant with given Value
+         *
+         * Returns UNKNOWN ENUM if not found
+         *
+         *@return Enum Constant
+         */
+        @JvmStatic
+        fun fromValue(value: String): MessagePayloadId {
+            return entries.find { it.value == value } ?: UNKNOWN
+        }
+    }
+}
+
+// Enum for Known Payload Events
+enum class PayloadEvent(val value: String) {
+    UNKNOWN("UNKNOWN"),
+
+    /* Requesting Values*/
+    /** Requests a List of Possible Values*/
+    @SerialName("list")
+    LIST("list"),
+
+    /** Requests current Single Value*/
+    @SerialName("peek")
+    PEEK("peek"),
+
+    /** Requests the image related to a Possible Value*/
+    @SerialName("thumb")
+    THUMB("thumb"),
+
+    /* Setting Values */
+    /** Sets a Single Value*/
+    @SerialName("set")
+    SET("set"),
+
+    /** Pushes a Single Value*/
+    @SerialName("push")
+    PUSH("push"),
+
+    /** Pops the last Pushed Single Value*/
+    @SerialName("pop")
+    POP("pop"),
+
+    /* Listening to Value Changes */
+    /** Listens to changes to a Channel Value*/
+    @SerialName("listen")
+    LISTEN("listen"),
+
+    /** Stops listening to changes to a Channel Value*/
+    @SerialName("unlisten")
+    UNLISTEN("unlisten"),
+    ;
+
+    companion object {
+        /**
+         * Gets Enum Constant with given Value
+         *
+         * Returns UNKNOWN ENUM if not found
+         *
+         *@return Enum Constant
+         */
+        @JvmStatic
+        fun fromValue(value: String): PayloadEvent {
+            return requireNotNull(entries.find { it.value == value }) { UNKNOWN }
+        }
+    }
+}
+
 
 
 
