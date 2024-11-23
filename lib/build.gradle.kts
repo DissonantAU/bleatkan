2fc3e3b91b7278d5644446cb2e79fcf381f07899
DissonantAU <33020221+DissonantAU@users.noreply.github.com>
2024-11-23 15:42:34 +1100

    Build Script Rework
    Now uses Tasks for building versions and copying to LibBuilds Folder
    
    Bump to Version 0.6.85 for testing builds

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.jvm.tasks.Jar
 import org.jetbrains.dokka.gradle.DokkaTask
 import java.net.URL
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
@@ -19,12 +18,31 @@ plugins {
 /* Version */
 val versionMajor: Int = 0
 val versionMinor: Int = 6
-val versionPatch: Int = 6 //Is padded with 0 to left if needed
+val versionPatch: Int = 85 //Is padded with 0 to left if needed
 
 
 group = "io.github.dissonantau"
 
 val buildsDir = rootProject.layout.projectDirectory.dir("libBuilds")
+println("Bleatkan LibBuilds Dir: $buildsDir")
+
+
+
+project.extra["releaseName"] = rootProject.name
+println("Release project: ${project.extra["releaseName"]}")
+
+// Version becomes 1203
+val versionCode: Int = versionMajor * 1000 + versionMinor * 100 + versionPatch
+project.extra["versionCode"] = versionCode
+println("Version Code: $versionCode")
+
+// Version Base Name becomes 1.2.03
+val versionName =
+    "$versionMajor.$versionMinor.${versionPatch.toString().padStart(2, '0')}"
+project.extra["versionBaseName"] = versionName
+
+calcVersion()
+
 
 repositories {
     mavenCentral()
@@ -32,7 +50,7 @@ repositories {
 
 dependencies {
     /* Main Dependencies */
-    //Kotlin BOM
+    // Kotlin BOM
     runtimeOnly(libs.kotlin.bom)
     implementation(platform(libs.kotlin.gradle.plugins.bom))
 
@@ -93,71 +111,79 @@ kotlin {
 }
 
 java {
-    //withJavadocJar()
-    //withSourcesJar()
-
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
 
-    register("calculateVersion") {
-
-        doFirst {
-            println("Is Release: ${(project.extra.has("IS_RELEASE") && project.extra["IS_RELEASE"] == true)}")
-
-            val isRelease =
-                System.getenv("IS_RELEASE") == "YES" ||
-                        (project.extra.has("IS_RELEASE") && project.extra["IS_RELEASE"] == true)
-            val versionSuffix: String = isRelease.ifFalse { "-DEV" }.orEmpty()
-
-            // Version becomes 1203
-            val versionCode: Int = versionMajor * 1000 + versionMinor * 100 + versionPatch
-            project.extra["versionCode"] = versionCode
-            println("Version Code: $versionCode")
-
-            // Version becomes 1.2.03 (Or 1.2.03-DEV etc.)
-            val versionName =
-                "$versionMajor.$versionMinor.${versionPatch.toString().padStart(2, '0')}$versionSuffix"
-            project.extra["versionName"] = versionName
-            project.version = versionName
-            println("Version Name: $versionName")
+    // Calculate Version/Build names, etc.
+    register("calculateLibraryVersion") {
+        doLast {
+            calcVersion()
         }
-
     }
 
+    // Run Version Calculation during Build, including IDE Import/refresh
+    // Mainly targeting prepareKotlinBuildScriptModel
+    //try {
+    //    rootProject.tasks.named("prepareKotlinBuildScriptModel") {
+    //        println("Add calculateLibraryVersion to dependsOn $name > Class ${javaClass.name}")
+    //        dependsOn(
+    //            named("calculateLibraryVersion")
+    //        )
+    //    }
+    //} catch (_: Throwable) {
+    //    // Add if prepareKotlinBuildScriptModel not found
+    //    println("prepareKotlinBuildScriptModel Not Found - Falling Back to Add to all")
+    //
+    //    rootProject.tasks.forEach {
+    //        if (it.name != "calculateLibraryVersion") {
+    //            println("Add calculateLibraryVersion to dependsOn $name > Class ${javaClass.name}")
+    //            it.dependsOn(
+    //                named("calculateLibraryVersion")
+    //            )
+    //        }
+    //    }
+    //}
+
+    /* Jar Tasks - Modify to generate version names and set archive properties */
 
     withType<Jar> {
         dependsOn(
-            named("calculateVersion")
+            named("calculateLibraryVersion")
         )
 
-        archiveBaseName.set(rootProject.name)
-        manifest {
-            attributes(
-                mapOf(
-                    "Implementation-Title" to rootProject.name,
-                    "Implementation-Version" to project.version
-                )
+        doFirst {
+            println(
+                "Project ${
+                    rootProject.name
+                } - archiveBaseName = ${
+                    project.extra["releaseName"]
+                }; archiveVersion = ${project.extra["versionName"]}"
             )
         }
+
+        archiveBaseName.set("${project.extra["releaseName"]}")
+        archiveVersion.set(provider { "${project.extra["versionName"]}" })
     }
 
+    /* Doc Generation */
 
     register<Jar>("dokkaHtmlJar") {
         group = "build"
+        archiveClassifier.set("html-docs")
         dependsOn(dokkaHtml)
         from(dokkaHtml.flatMap { it.outputDirectory })
-        archiveClassifier.set("html-docs")
     }
 
     register<Jar>("dokkaJavadocJar") {
         group = "build"
+        archiveClassifier.set("javadoc")
         dependsOn(dokkaJavadoc)
         from(dokkaJavadoc.flatMap { it.outputDirectory })
-        archiveClassifier.set("javadoc")
     }
+
 
     // From https://github.com/Kotlin/dokka/blob/1.9.20/examples/gradle/dokka-gradle-example/build.gradle.kts
     withType<DokkaTask>().configureEach {
@@ -179,54 +205,40 @@ tasks {
                         )
                     )
 
-                    println("Local Source Link: ${localDirectory.get()}")
-                    println("Remote Source Link: ${remoteUrl.get()}")
+                    //println("Local Source Link: ${localDirectory.get()}")
+                    //println("Remote Source Link: ${remoteUrl.get()}")
                 }
             }
         }
     }
 
 
-    register("setBuildDev") {
-        doFirst {
-            println("Set to Non-Release Build")
-            project.ext["IS_RELEASE"] = false
-        }
-    }
-
-    register("setBuildRelease") {
-        doFirst {
-            println("Set to Release Build")
-            project.ext["IS_RELEASE"] = true
-        }
-    }
-
-    /* Task to build the project, copy to libBuilds*/
+    /* Tasks to build the project/docs & copy to libBuilds*/
 
     register<Copy>("copyToLibBuilds") {
         duplicatesStrategy = DuplicatesStrategy.WARN
 
         doFirst {
-            println("Copy to '${buildsDir.dir("${rootProject.name}-${project.version}")}'")
+            println(
+                "Copy ${rootProject.name} to '${
+                    buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}")
+                }'"
+            )
         }
 
         mustRunAfter(
-            named("calculateVersion")
+            named("calculateLibraryVersion")
         )
         dependsOn(
-            build,
+            //build,
             jar,
             kotlinSourcesJar,
-            named<Jar>("dokkaJavadocJar"),
-            named<Jar>("dokkaHtmlJar"),
         )
         from(
             jar,
             kotlinSourcesJar,
-            named<Jar>("dokkaJavadocJar"),
-            named<Jar>("dokkaHtmlJar"),
         )
-        into { buildsDir.dir("${rootProject.name}-${project.version}") }
+        into { buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}") }
 
     }
 
@@ -235,67 +247,102 @@ tasks {
         duplicatesStrategy = DuplicatesStrategy.WARN
 
         doFirst {
-            println("Copy to '${buildsDir.dir("${rootProject.name}-${project.version}").dir("docs").dir("html")}'")
+            println(
+                "Copy to '${
+                    buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}")
+                        .dir("docs")
+                        .dir("html")
+                }'"
+            )
         }
 
-        mustRunAfter(named("calculateVersion"))
+        mustRunAfter(named("calculateLibraryVersion"))
         dependsOn(dokkaHtml)
         from(dokkaHtml)
-        into { buildsDir.dir("${rootProject.name}-${project.version}").dir("docs").dir("html") }
-
+        into {
+            buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}")
+                .dir("docs")
+                .dir("html")
+        }
     }
 
     register<Copy>("copyToLibBuildsJavadoc") {
         duplicatesStrategy = DuplicatesStrategy.WARN
 
         doFirst {
-            println("Copy to '${buildsDir.dir("${rootProject.name}-${project.version}").dir("docs").dir("javadoc")}'")
+            println(
+                "Copy to '${
+                    buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}")
+                        .dir("docs")
+                        .dir("javadoc")
+                }'"
+            )
         }
 
-        mustRunAfter(named("calculateVersion"))
+        mustRunAfter(named("calculateLibraryVersion"))
         dependsOn(dokkaJavadoc)
         from(dokkaJavadoc)
-        into { buildsDir.dir("${rootProject.name}-${project.version}").dir("docs").dir("javadoc") }
+        into {
+            buildsDir.dir("${project.extra["releaseName"]}-${project.extra["versionName"]}").dir("docs").dir("javadoc")
+        }
+    }
+
+    /* Meta Build Jobs */
+
+    register("buildCopySnapshotToLibBuilds") {
+        group = "build"
+
+        doFirst {
+            println("Building ${rootProject.name} as Snapshot Build")
+            project.ext["BUILD_TYPE"] = "SNAPSHOT"
+        }
+
+        finalizedBy(
+            named("calculateLibraryVersion"),
+            named("copyToLibBuilds"),
+            //named("copyToLibBuildsHtml"),
+            //named("copyToLibBuildsJavadoc"),
+        )
     }
 
     register("buildCopyDevToLibBuilds") {
         group = "build"
 
         doFirst {
-            println("Set to Dev Build")
-            project.ext["IS_RELEASE"] = false
+            println("Building ${rootProject.name} as Dev Build")
+            project.ext["BUILD_TYPE"] = "DEV"
         }
 
         finalizedBy(
-            named("calculateVersion"),
+            named("calculateLibraryVersion"),
             named("copyToLibBuilds"),
             named("copyToLibBuildsHtml"),
             named("copyToLibBuildsJavadoc"),
         )
-
     }
 
     register("buildCopyReleaseToLibBuilds") {
         group = "build"
 
         doFirst {
-            println("Set to Release Build")
-            project.ext["IS_RELEASE"] = true
+            println("Building ${rootProject.name} as Release Build")
+            project.ext["BUILD_TYPE"] = "RELEASE"
         }
 
         finalizedBy(
-            named("calculateVersion"),
+            named("calculateLibraryVersion"),
             named("copyToLibBuilds"),
             named("copyToLibBuildsHtml"),
             named("copyToLibBuildsJavadoc"),
         )
-
     }
+
 
     test {
         useJUnitPlatform()
     }
 }
+
 
 publishing {
     publications {
@@ -311,4 +358,48 @@ publishing {
             url = uri(layout.buildDirectory.dir("repo"))
         }
     }
+}
+
+
+fun calcVersion() {
+    println("Release Name: ${rootProject.name}")
+    // Get Environment Var if it exists
+    val envBuildType: String = try {
+        System.getenv("BUILD_TYPE")
+    } catch (_: Throwable) {
+        ""
+    }
+
+    // Release Type - Variable from Build Tasks > Environment Var > Snapshot Default
+    val buildType: String =
+        when {
+            project.extra.has("BUILD_TYPE") -> "${project.extra["BUILD_TYPE"]}"
+            envBuildType.isNotBlank() -> System.getenv("BUILD_TYPE")
+            else -> "SNAPSHOT"
+        }
+
+    println("Release Type: $buildType")
+
+    val buildSuffix: String = when (buildType) {
+        "RELEASE" -> ""
+        "DEV" -> "-DEV"
+        else -> "-SNAPSHOT"
+    }
+
+    // Version Name becomes 1.2.03 or 1.2.03-DEV etc.
+    val versionNameSuffix = "${project.extra["versionBaseName"]}$buildSuffix"
+
+    project.extra["versionName"] = versionNameSuffix
+    project.version = versionNameSuffix
+    println("Version Full Name: $versionNameSuffix")
+
+    java.manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to rootProject.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+
 }
